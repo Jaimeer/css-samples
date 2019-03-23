256ee906c00cd4252d57750da999e98a572a8e2e
Jaime Escolar <jaime.esccolar@gmail.com>
2019-03-23 12:48:42 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu :items="samples" @changeItem="changeItem"/>
+    <component :is="component"></component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/Menu.vue'
+import Sample1 from './samples/Sample1.vue'
+import Sample2 from './samples/Sample2.vue'
+import Sample3 from './samples/Sample3.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'App',
+  components: { Menu, Sample1, Sample2, Sample3 },
+  data: () => ({
+    samples: [
+      { component: 'Sample1', text: 'Menu1' },
+      { component: 'Sample2', text: 'Menu2' },
+      { component: 'Sample3', text: 'Smoke' },
+    ],
+    component: Sample3,
+  }),
+  methods: {
+    changeItem(item) {
+      this.component = item.component
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="menu">
+    <ul>
+      <li v-for="item in items" :key="item.component">
+        <a href="#" @click="changeItem(item)">{{item.text}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: () => ({}),
+  methods: {
+    changeItem(item) {
+      this.$emit('changeItem', item)
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900');
+.menu {
+  display: flex;
+  place-items: center;
+  justify-content: center;
+  font-family: 'Poopins', sans-serif;
+  font-weight: 600;
+  background: #fff;
+
+  ul {
+    padding: 0;
+    margin: 0;
+    margin: 10px 0;
+  }
+
+  li {
+    list-style: none;
+    display: inline-block;
+    padding: 10px;
+
+    a {
+      text-decoration: none;
+      border: 1px #000 solid;
+      padding: 10px 20px;
+      text-transform: uppercase;
+      color: #000;
+    }
+    a:hover {
+      background: gray;
+      color: #fff;
+      border: 1px #fff solid;
+    }
+  }
+}
+</style>
